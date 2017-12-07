f5b762c48a8a696cddb0376c5b330b1a5a37d058
M Matthew Hydock <mhydock@yahoo.com>
2017-12-06 21:26:56 -0500

    Fixing dumb mistakes

--- src/main/java/com/asymptote/gamelib/physics/Collidable.java
+++ src/main/java/com/asymptote/gamelib/physics/Collidable.java
@@ -1,5 +1,6 @@
-package com.asymptote.gamelib.core;
+package com.asymptote.gamelib.physics;
 
+import com.asymptote.gamelib.core.Transformable;
 import org.joml.Vector3f;
 
 public abstract class Collidable extends Transformable {
@@ -10,22 +11,22 @@ public abstract class Collidable extends Transformable {
     public float getWeight() { return this.weight; }
     public void setWeight(float weight) { this.weight = weight; }
 
-    public float getVelocity() { return this.velocity; }
-    public float setVelocityX(float x) { this.velocity.x = x; }
-    public float setVelocityY(float y) { this.velocity.y = y; }
-    public float setVelocityZ(float z) { this.velocity.z = z; }
-    public float setVelocity(float x, float y, float z)
+    public Vector3f getVelocity() { return this.velocity; }
+    public void setVelocityX(float x) { this.velocity.x = x; }
+    public void setVelocityY(float y) { this.velocity.y = y; }
+    public void setVelocityZ(float z) { this.velocity.z = z; }
+    public void setVelocity(float x, float y, float z)
     {
         this.velocity.x = x;
         this.velocity.y = y;
         this.velocity.z = z;
     }
 
-    public float getRotVelocity() { return this.rotVelocity; }
-    public float setRotVelocityX(float x) { this.rotVelocity.x = x; }
-    public float setRotVelocityY(float y) { this.rotVelocity.y = y; }
-    public float setRotVelocityZ(float z) { this.rotVelocity.z = z; }
-    public float setRotVelocity(float x, float y, float z)
+    public Vector3f getRotVelocity() { return this.rotVelocity; }
+    public void setRotVelocityX(float x) { this.rotVelocity.x = x; }
+    public void setRotVelocityY(float y) { this.rotVelocity.y = y; }
+    public void setRotVelocityZ(float z) { this.rotVelocity.z = z; }
+    public void setRotVelocity(float x, float y, float z)
     {
         this.rotVelocity.x = x;
         this.rotVelocity.y = y;
